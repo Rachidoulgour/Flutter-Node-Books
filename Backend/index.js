b60e8668a5b8a3caa1ce0e3b1d1b723a432a6c2a
rachid <olgor@hotmail.com>
2021-03-07 18:02:10 +0100

    database added

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,12 @@
 const app = require('./app');
+const startConnection = require('./database');
 
 
 
 
 
 async function main(){
-    
+    startConnection(),
 
     app.listen(app.get('port'))
     console.log('Server on port', app.get('port'))
